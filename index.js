ba414fa04d311d2565294f9c381c5e22e180abc8
RoutedandSwitched <60159972+RoutedandSwitched@users.noreply.github.com>
2022-05-16 20:15:02 -0500

    Finished

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ function roundToThree(n){
 
 function convertVal(){
     let userInput = document.getElementById("user-input").value
-    document.getElementById("length-m").textContent = userInput + " Meters = " + 
+    document.getElementById("length-meter").textContent = userInput + " Meters = " + 
         roundToThree(userInput*3.281) + " Feet"
-    document.getElementById("length-ft").textContent = userInput + " Feet = "+ 
+    document.getElementById("length-feet").textContent = userInput + " Feet = "+ 
         roundToThree(userInput/3.281) + " Meters"
+    document.getElementById("vol-liter").textContent = userInput + " Liters = "+ 
+        roundToThree(userInput/3.7854) + " Gallons"
+    document.getElementById("vol-gal").textContent = userInput + " Gallons = "+ 
+        roundToThree(userInput*3.7854) + " Liters"
+    document.getElementById("mass-kilo").textContent = userInput + " Kilos = "+ 
+        roundToThree(userInput*2.204623) + " Pounds"
+    document.getElementById("mass-lbs").textContent = userInput + " Pounds = "+ 
+        roundToThree(userInput/3.7854) + " Kilos"
 }
 console.log(lengthm_el)
 convertVal()
